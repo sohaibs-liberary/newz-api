37cda17bb8d9940daffc56d53a7424724a82d998
sohaibs-liberary <muhammadsohaibshakeel8gmail.com>
2025-08-23 00:52:15 +0500

    make next page

--- src/App.jsx
+++ src/App.jsx
@@ -18,35 +18,43 @@ function App() {
   return (
     <>
 
-      <div  id="allposts" className=' bg-white p-6 text-blue-500 ' >
+      <div id="allposts" className=' bg-white p-6 text-blue-500 ' >
         <h1 className='newzapp' >News App</h1>
         <h2 className='latestnewz'>Latest News</h2>
         <div className="post">
-       
-        {
-          data.results ? data.results.map((posts , index)=>(
-            <div key={index} className='post-detail' >
-              {/* <h2>id:{posts.article_id}</h2> */}
-              <h1>{posts.title}</h1>
-            {
-              posts.description && <p>description: {posts.description}</p>
-            }
-           {
-               posts.image_url && <img src={posts.image_url} alt={posts.title} className='img  rounded-full'/>
 
-             }
-             <br/>
-             {
-              posts.link && <a href={posts.link}>Read More</a>
-             }
+          {
+            data.results ? data.results.map((posts, index) => (
+              <div key={index} className='post-detail' >
+                {/* <h2>id:{posts.article_id}</h2> */}
+                <h1>{posts.title}</h1>
+                {
+                  posts.description && <p>{posts.description}</p>
+                }
+                {
+                  posts.image_url && <img src={posts.image_url} alt={posts.title} className='img  rounded-full' />
+
+                }
+                <br />
+                {
+                  posts.link && <a href={posts.link}>Read More</a>
+                }
+                
+
+                
+
               </div>
-           )) : (
-            <div>
-              <p>loading...</p>
-            </div>
-          )
-        } 
-        
+              
+            )) : (
+              <div>
+
+                <p>loading...</p>
+                
+              </div>
+
+            )}
+
+
         </div>
       </div>
     </>
